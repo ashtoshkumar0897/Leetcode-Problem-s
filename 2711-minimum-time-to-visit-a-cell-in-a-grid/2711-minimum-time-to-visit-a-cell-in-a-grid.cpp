41c15cc238a8bbc4177d3a962d6c1024ad73fd5f
ashutoshkumar0897 <139233867+ashtoshkumar0897@users.noreply.github.com>
2024-11-29 12:06:20 +0530

    Time: 1110 ms (7.36%), Space: 201.6 MB (13.75%) - LeetHub

---
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/2711-minimum-time-to-visit-a-cell-in-a-grid.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minimumTime(vector<vector<int>>& grid) {
+        if (min(grid[0][1],grid[1][0]) > 1){
+            return -1;
+        }
+
+        int ROWS = grid.size(),COLS = grid[0].size();
+        priority_queue<vector<int>, vector<vector<int>>, greater<>> minHeap;
+        minHeap.push({0,0,0}); // {time,r,c}
+        set<pair<int,int>> visit;
+
+        while (!minHeap.empty()){
+            auto curr = minHeap.top();
+            minHeap.pop();
+            int t =curr[0], r =curr[1], c= curr[2];
+
+            if (r==ROWS -1 && c ==COLS -1){
+                return t;
+            }
+            vector<pair<int, int >> dirs ={{1,0},{-1,0},{0,1},{0,-1}};
+            for(auto [dr,dc] : dirs){
+                int nr = r+dr,nc =c+dc;
+                if(nr <0 || nc <0 || nr == ROWS|| nc ==COLS|| visit.count({nr,nc})){
+                    continue;
+                }
+
+                int wait = (abs(grid[nr][nc] -t)% 2==0)? 1:0;
+                int nTime=max(grid[nr][nc] + wait,t +1);
+                minHeap.push({nTime,nr,nc});
+                visit.insert({nr,nc});
+            }
+        }
+        return -1;
+    }
+};
