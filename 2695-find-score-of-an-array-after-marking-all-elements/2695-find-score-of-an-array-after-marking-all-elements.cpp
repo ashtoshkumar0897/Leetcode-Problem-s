e87d7a149c9ed99fd90ead3c628f50644ba72352
ashutoshkumar0897 <139233867+ashtoshkumar0897@users.noreply.github.com>
2024-12-13 13:15:11 +0530

    Time: 119 ms (69.68%), Space: 105.6 MB (69.61%) - LeetHub

--- 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
     long long findScore(vector<int>& nums) {
-        int n = nums.size();  // Get the size of the array nums
-        vector<int> ord(n);  // Array to store the indices of nums
-        iota(ord.begin(), ord.end(), 0);  // Initialize ord with 0, 1, 2, ..., n-1
+        int n = nums.size();
 
-        // Sort the indices based on the values in nums
-        sort(ord.begin(), ord.end(), [&](int a, int b) {
-            if (nums[a] != nums[b])
-                return nums[a] < nums[b];  // Sort by value of nums
-            return a < b;  // If values are the same, sort by index
-        });
+        vector<bool> marked(n, false);
 
-        vector<bool> m(n, false);  // Array to mark which elements are already processed
-        long long score = 0;  // Initialize score to 0
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        for (int i = 0; i < n; i++)
+            pq.emplace(nums[i], i);
 
-        // Iterate over the sorted indices to calculate the score
-        for (int x : ord) {
-            if (m[x])  // If the element is already marked, skip it
+        long long ans = 0;
+        while (!pq.empty()) {
+            pair<int, int> curr = pq.top();
+            pq.pop();
+            if (marked[curr.second])
                 continue;
-
-            score += nums[x];  // Add the value of nums[x] to the score
-            m[x] = true;  // Mark the current element as processed
-
-            // Mark the previous element as processed, if it exists
-            if (x > 0)
-                m[x - 1] = true;
-
-            // Mark the next element as processed, if it exists
-            if (x + 1 < n)
-                m[x + 1] = true;
+            ans += curr.first;
+            if (curr.second + 1 < n)
+                marked[curr.second + 1] = true;
+            if (curr.second - 1 >= 0)
+                marked[curr.second - 1] = true;
         }
 
-        return score;  // Return the total score
+        return ans;
     }
 };
