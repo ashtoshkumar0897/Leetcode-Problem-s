9f79c714be54b97f4dd3d91d5aaff485041227a9
ashutoshkumar0897 <139233867+ashtoshkumar0897@users.noreply.github.com>
2025-01-12 11:25:00 +0530

    Time: 20 ms (25.13%), Space: 34.6 MB (24.37%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,52 +1,23 @@
 class Solution {
 public:
-    bool canBeValid(string s, string locked) {
-        int length = s.size();
-        // If length of string is odd, return false.
-        if (length % 2 == 1) {
-            return false;
-        }
-        int openBrackets = 0, unlocked = 0;
-        // Iterate through the string to handle '(' and ')'.
-        for (int i = 0; i < length; i++) {
-            if (locked[i] == '0') {
-                unlocked++;
-            } else if (s[i] == '(') {
-                openBrackets++;
-            } else if (s[i] == ')') {
-                if (openBrackets > 0) {
-                    openBrackets--;
-                } else if (unlocked > 0) {
-                    unlocked--;
-                } else {
-                    return false;
-                }
+    bool canBeValid(string s, string l) {
+        int n = s.size();
+        stack<int> st,stt;
+        for(int i = 0;i<n;i++){
+            if(l[i] == '0') st.push(i);
+            else{
+               if(s[i] == ')'){
+                  if(st.empty() && stt.empty()) return 0;
+                  if(stt.empty()) st.pop();
+                  else stt.pop();
+               }
+               else stt.push(i);
             }
         }
-        // Match remaining open brackets with unlocked characters.
-        int balance = 0;
-        for (int i = length - 1; i >= 0; i--) {
-            if (locked[i] == '0') {
-                balance--;
-                unlocked--;
-            } else if (s[i] == '(') {
-                balance++;
-                openBrackets--;
-            } else if (s[i] == ')') {
-                balance--;
-            }
-            if (balance > 0) {
-                return false;
-            }
-            if (unlocked == 0 and openBrackets == 0) {
-                break;
-            }
+        while(!st.empty() && !stt.empty() && stt.top() < st.top()){
+            st.pop(); stt.pop();
         }
-
-        if (openBrackets > 0) {
-            return false;
-        }
-
-        return true;
+        if(!stt.empty()) return 0;
+        return (st.size()%2 == 0);
     }
 };
