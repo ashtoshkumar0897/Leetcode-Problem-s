98a0412a217d29604deaff0e1220227c9ef362ca
ashutoshkumar0897 <139233867+ashtoshkumar0897@users.noreply.github.com>
2025-06-21 18:39:14 +0530

    Time: 0 ms (100%), Space: 19.4 MB (70.29%) - LeetHub

--- 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    int minimumDeletions(string word, int k) {
-        unordered_map<char,int>cnt;
-        for(auto &ch: word){
-            cnt [ch]++;
+   int minimumDeletions(string word, int k) {
+    vector<int> freq(26, 0);
+    int deleted = 0, ans = word.size();
+    for(auto c: word) freq[c-'a']++;             // find frequency of each chars
+    sort(freq.begin(), freq.end());              // sort the freq array
+    for(int i = 0; i < freq.size(); ++i){        // Iterate over freq 
+        int res = deleted, minFreq = freq[i];    // consider the current freq as the min freq after deletion. Also add the already deleted freq to temporary result
+        for(int j = freq.size()-1; j > i; --j){  // iterate over freq array to evaluate the chars to be deleted from the higher freq side of the freq array
+            if(freq[j] - minFreq <= k) break;    // Once condition is satisfied then exit from the loop
+            res += freq[j] - minFreq - k;        // accumulate on the result - number of items to be deleted
         }
-        int res = word.size();
-        for(auto &[_,a] : cnt){
-            int deleted = 0;
-            for(auto &[_,b] : cnt){
-                if (a>b){
-                    deleted += b;
-                }else if (b > a + k){
-                    deleted += b - (a + k);
-                }
-            }
-            res = min(res,deleted);
-        }
-        return res;
-        
+        ans = min(ans, res);                     // keep tracking the min possible value
+        deleted += freq[i];                      // update the deleted freq with current freq
     }
+    return ans;
+}
 };
