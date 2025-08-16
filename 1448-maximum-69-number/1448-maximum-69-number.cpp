e2f4b5fe629beb7850c01e0e72069de86ed3ad40
ashutoshkumar0897 <139233867+ashtoshkumar0897@users.noreply.github.com>
2025-08-16 19:19:16 +0530

    Time: 0 ms (100%), Space: 8.1 MB (41.82%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        int placeValue =0;
+        int placeValueSix = -1;
+
+        int temp = num;
+        while(temp >0){
+            int remain = temp%10;
+
+            if(remain ==6)
+                placeValueSix = placeValue;
+            temp = temp/10;
+            placeValue++;
+        }
+
+        if(placeValueSix ==-1)// we never got 6 in the num
+            return num;
+        return num + 3 * pow(10,placeValueSix);
+        
+    }
+};
