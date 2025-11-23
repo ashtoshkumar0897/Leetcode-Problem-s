0ee271fe0116103d07ca95a0fdb365f23466f2d5
ashutoshkumar0897 <139233867+ashtoshkumar0897@users.noreply.github.com>
2025-11-23 20:31:09 +0530

    Time: 28 ms (54.37%), Space: 46 MB (13.83%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,3 +1,4 @@
+/*
 //Approach -1 - greedily using %3 property
 // T.C : O(nlogn)
 //S.c : O(n)
@@ -37,5 +38,36 @@ public:
     return result < 0 ? 0 : result;
 
         
+    }
+};
+
+
+*/
+
+
+//Approach -2 - recursion Memoization
+// T.C : O(n)
+// S.C : O(n)
+class Solution {
+public:
+    int solve(int i , int remainder, vector<int>& nums, vector<vector<int>>& t){
+        if(i >= nums.size()){
+            return (remainder == 0) ? 0 : INT_MIN;
+        }
+        if(t[i][remainder] != -1)
+            return t[i][remainder];
+
+         // Pick the current number
+        int pick = nums[i] +
+                   solve(i + 1, (remainder + nums[i]) % 3, nums, t);
+
+        // Skip the current number
+        int notPick = solve(i + 1,remainder,nums,t);
+        return t[i][remainder] = max(pick, notPick);
+    }
+    int maxSumDivThree(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> t(n , vector<int>(3,-1));
+        return solve(0,0,nums,t);
     }
 };
