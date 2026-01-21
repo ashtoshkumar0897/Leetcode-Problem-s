99a54750a63a9f3967c01a896b6b5416686e2ea7
ashutoshkumar0897 <139233867+ashtoshkumar0897@users.noreply.github.com>
2026-01-21 10:55:45 +0530

    Time: 0 ms (100%), Space: 28.4 MB (9.71%) - LeetHub

---
+++ 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
@@ -0,0 +1,32 @@
+//Approach (Brute Force All Possible Answers
+// T.C : O(n)
+// S.C : O(n)
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> result;
+
+        for(int &num : nums){
+            if(num == 2){
+                result.push_back(-1);
+                continue;
+            }
+            bool found = false;
+            for(int j =1; j<32;j++){
+                if((num &(1 <<j))>0){ // set bit
+                    continue;
+                }
+
+                // We found an unset bit at jth position
+                result.push_back((num ^ (1 <<(j-1)))); // set the first set bit to 0
+                found = true;
+                break;
+            }
+
+            if(!found)
+                result.push_back(-1);
+        }
+        return result;
+        
+    }
+};
