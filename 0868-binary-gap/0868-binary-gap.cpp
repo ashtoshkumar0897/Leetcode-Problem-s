348b6c2b68db4c6352fd73cd0b0c89563519d04e
ashutoshkumar0897 <139233867+ashtoshkumar0897@users.noreply.github.com>
2026-02-22 11:45:08 +0530

    Time: 0 ms (100%), Space: 8.1 MB (44.66%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.cpp
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -5,14 +5,12 @@ public:
         int prev =-1;
 
         int result =0;
-        while(n >0){
-            if((n&1)>0){
-                result = (prev != -1) ? max(result,curr - prev) : result;
-                prev = curr;
-            }
-             curr++;
-             n >>= 1;
+       for(int curr =0; curr <32;curr++){
+        if(((n >> curr)& 1)>0) {
+            result =(prev != -1) ? max(result,curr - prev) : result;
+            prev = curr;
         }
+       }
          return result;
         
     }
