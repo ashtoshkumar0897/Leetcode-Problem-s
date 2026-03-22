bee22f8d2828bf92db2756166ad748586e1ec2c9
ashutoshkumar0897 <139233867+ashtoshkumar0897@users.noreply.github.com>
2026-03-22 10:02:57 +0530

    Time: 0 ms (100%), Space: 14.7 MB (41.55%) - LeetHub

---
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+//Approach (Rotate and check till 4 rotations)
+//T.c: O(n^2)
+//S.C :O(1)
+
+    int n;
+    void rotate(vector<vector<int>>& mat){
+        // Transpose
+        
+        for(int i =0; i< n; i++){
+            for(int j = i; j < n; j++){
+                swap(mat[i][j], mat[j][i]);
+            }
+        }
+
+        // Reverse each row
+
+        for(int  i= 0; i < n; i++){
+            reverse(mat[i].begin(),mat[i].end());
+        }
+    }
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        n = mat.size();
+
+        for(int c =1; c<=4;c++){
+            bool equal = true;
+            for(int i = 0; i<n;i++){
+                for(int j =0;j<n;j++){
+                    if(mat[i][j] != target[i][j]){
+                        equal = false;
+                        break;
+                    }
+                }
+                if(!equal)
+                break;
+            }
+            if(equal)
+            return true;
+
+            rotate(mat);
+        }
+        return false;
+        
+    }
+};
