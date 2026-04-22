8003a7c6e43f329eac0ce1de662ac15882ee007c
ashutoshkumar0897 <139233867+ashtoshkumar0897@users.noreply.github.com>
2026-04-22 14:11:51 +0530

    Time: 0 ms (100%), Space: 12.4 MB (97.33%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -1,64 +1,23 @@
-struct TrieNode {
-    TrieNode* child[26];
-    bool isEnd;
-    TrieNode() {
-        memset(child, 0, sizeof(child));
-        isEnd = false;
-    }
-};
-
 class Solution {
 public:
-    TrieNode* root = new TrieNode();
-
-    void insert(string& word) {
-        TrieNode* node = root;
-        for (char c : word) {
-            int idx = c - 'a';
-            if (!node->child[idx]) node->child[idx] = new TrieNode();
-            node = node->child[idx];
-        }
-        node->isEnd = true;
-    }
-
-    bool dfs(string& word, int i, TrieNode* node, int cnt) {
-        if (cnt > 2) return false;
-        if (!node) return false;
-
-        if (i == word.size()) {
-            return node->isEnd;
-        }
-
-        int idx = word[i] - 'a';
-
-        // no changes made
-        if (node->child[idx]) {
-            if (dfs(word, i + 1, node->child[idx], cnt)) return true;
-        }
-
-        // made changes
-        if (cnt < 2) {
-            for (int c = 0; c < 26; c++) {
-                if (c == idx) continue;
-                if (node->child[c]) {
-                    if (dfs(word, i + 1, node->child[c], cnt + 1)) return true;
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        int n=queries[0].size();
+        vector<string> ans;
+        for(auto &it:queries){
+            for(auto &i:dictionary){
+                int diff=0;
+                for(int idx=0; idx<n; idx++){
+                    if(it[idx]!=i[idx]){
+                        diff++;
+                    }
+                    if(diff>2) break;
+                }
+                if(diff<=2){
+                    ans.push_back(it);
+                    break;
                 }
             }
         }
-
-        return false;
-    }
-
-    vector<string> twoEditWords(vector<string>& queries,
-                                vector<string>& dictionary) {
-        for (auto& w : dictionary) insert(w);
-
-        vector<string> res;
-        for (auto& q : queries) {
-            if (dfs(q, 0, root, 0)) {
-                res.push_back(q);
-            }
-        }
-        return res;
+        return ans;
     }
 };
