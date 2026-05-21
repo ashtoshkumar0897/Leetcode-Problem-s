1c29859bd4c777250aae3f031eacfae1b9766c87
Ashutosh Kumar <139233867+ashtoshkumar0897@users.noreply.github.com>
2026-05-21 19:16:12 +0530

    Time: 126 ms (89.33%), Space: 160.7 MB (43.5%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,26 +1,67 @@
+struct TrieNode{
+    //char digits;
+    TrieNode* children[10];
+};
+
+
 class Solution {
 public:
+
+    TrieNode* getTrieNode(){
+        TrieNode* node = new TrieNode();
+        for(int i =0; i<10;i++){
+            node->children[i] = nullptr;
+        }
+        return node;
+    }
+
+    void insert(int num, TrieNode* root){
+        TrieNode* crawl = root;
+        string numStr = to_string(num);
+
+        for(char ch :numStr){
+            int idx = ch -'0';
+            if(!crawl -> children[idx]){
+                crawl ->children[idx] = getTrieNode();
+            }
+
+            crawl = crawl->children[idx];
+        }
+
+    }
+
+
+    int  search(int num,TrieNode* root){
+        TrieNode* crawl = root;
+        string numStr = to_string(num);
+        int length =0;
+
+        for(char ch : numStr){
+            int idx = ch -'0';
+            if(crawl->children[idx]){
+                length++;
+                crawl = crawl ->children[idx];
+            }else{
+                break;
+            }
+        }
+        return length;
+
+
+
+    }
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        unordered_set<int>st;
+      TrieNode* root = getTrieNode();
+
+      for(int num : arr1){
+        insert(num, root);
+      }
         
-        for(int val:arr1){
-            while(!st.count(val) && val >0){
-                st.insert(val);
-
-                val = val/10;
-            }
-        }
-
         int result =0;
-        for(int num:arr2){
-            while(!st.count(num) && num >0){
-                num /=10;
-            }
-
-            if(num > 0){
-                result = max(result,static_cast<int>(log10(num) +1));
-            }
+        for(int num : arr2){
+            result = max(result,search(num,root));
         }
+
         return result;
     }
 };
