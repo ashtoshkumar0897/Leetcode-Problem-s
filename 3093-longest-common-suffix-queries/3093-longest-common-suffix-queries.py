8299b505f5b048b55a06b5a6a13a850c9ae03cfb
Ashutosh Kumar <139233867+ashtoshkumar0897@users.noreply.github.com>
2026-05-28 11:12:44 +0530

    Time: 1692 ms (30.19%), Space: 180.5 MB (40.88%) - LeetHub

--- 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.py
+++ 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.py
@@ -1,55 +1,55 @@
-from typing import List
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.min_len = float("inf")
+        self.idx = float("inf")
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, s: str, idx: int):
+        node = self.root
+        if len(s) < node.min_len:
+            node.min_len = len(s)
+            node.idx = idx
+
+        for ch in s:
+            c = ch
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+
+            if len(s) < node.min_len:
+                node.min_len = len(s)
+                node.idx = idx
+
+    def query(self, s: str) -> int:
+        node = self.root
+
+        for ch in s:
+            if ch in node.children:
+                node = node.children[ch]
+            else:
+                break
+
+        return node.idx
+
 
 class Solution:
-    class TrieNode:
-        def __init__(self, idx: int):
-            self.idx = idx
-            self.children = [None] * 26
+    def stringIndices(
+        self, wordsContainer: List[str], wordsQuery: List[str]
+    ) -> List[int]:
+        trie = Trie()
 
-    def getNode(self, i: int) -> 'Solution.TrieNode':
-        return Solution.TrieNode(i)
+        for i, word in enumerate(wordsContainer):
+            reversed_word = word[::-1]
+            trie.insert(reversed_word, i)
 
-    def insertTrie(self, pCrawl: 'Solution.TrieNode', i: int, wordsContainer: List[str]):
-        word = wordsContainer[i]
-        n = len(word)
+        res = []
+        for query in wordsQuery:
+            reversed_query = query[::-1]
+            res.append(trie.query(reversed_query))
 
-        for j in range(n - 1, -1, -1):  # traverse backwards
-            ch_idx = ord(word[j]) - ord('a')
-
-            if pCrawl.children[ch_idx] is None:
-                pCrawl.children[ch_idx] = self.getNode(i)
-
-            pCrawl = pCrawl.children[ch_idx]
-
-            if len(wordsContainer[pCrawl.idx]) > n:
-                pCrawl.idx = i
-
-    def search(self, pCrawl: 'Solution.TrieNode', word: str) -> int:
-        result_idx = pCrawl.idx
-        n = len(word)
-
-        for i in range(n - 1, -1, -1):
-            ch_idx = ord(word[i]) - ord('a')
-            if pCrawl.children[ch_idx] is None:
-                return result_idx
-            pCrawl = pCrawl.children[ch_idx]
-            result_idx = pCrawl.idx
-
-        return result_idx
-
-    def stringIndices(self, wordsContainer: List[str], wordsQuery: List[str]) -> List[int]:
-        m = len(wordsContainer)
-        n = len(wordsQuery)
-        result = [0] * n
-
-        root = self.getNode(0)
-
-        for i in range(m):
-            if len(wordsContainer[root.idx]) > len(wordsContainer[i]):
-                root.idx = i
-            self.insertTrie(root, i, wordsContainer)
-
-        for i in range(n):
-            result[i] = self.search(root, wordsQuery[i])
-
-        return result
+        return res
