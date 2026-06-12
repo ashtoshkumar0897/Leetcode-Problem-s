10fbe2cb8f58f0ea2dae2d67e79aa377546221dd
Ashutosh Kumar <139233867+ashtoshkumar0897@users.noreply.github.com>
2026-06-12 21:35:26 +0530

    Time: 1497 ms (28.3%), Space: 106.4 MB (66.04%) - LeetHub

--- 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.py
+++ 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.py
@@ -1,82 +1,89 @@
-import math
-from typing import List
-
-
-class LCA:
-    def __init__(self, edges: List[List[int]], root: int = 1):
-        self.n = len(edges) + 1
-        self.m = int(math.log2(self.n)) + 2
-        self.e = [[] for _ in range(self.n + 1)]
-        self.d = [0] * (self.n + 1)
-        self.f = [[0] * self.m for _ in range(self.n + 1)]
-
-        for u, v in edges:
-            self.e[u].append(v)
-            self.e[v].append(u)
-
-        self.dfs(root, 0)
-
-        for i in range(1, self.m):
-            for x in range(1, self.n + 1):
-                self.f[x][i] = self.f[self.f[x][i - 1]][i - 1]
-
-    def dfs(self, x: int, fa: int):
-        self.f[x][0] = fa
-        for y in self.e[x]:
-            if y == fa:
-                continue
-            self.d[y] = self.d[x] + 1
-            self.dfs(y, x)
-
-    def lca(self, x: int, y: int) -> int:
-        if self.d[x] > self.d[y]:
-            x, y = y, x
-
-        # raise y to the same depth as x
-        diff = self.d[y] - self.d[x]
-        for i in range(self.m - 1, -1, -1):
-            if diff & (1 << i):
-                y = self.f[y][i]
-
-        if x == y:
-            return x
-
-        for i in range(self.m - 1, -1, -1):
-            if self.f[x][i] != self.f[y][i]:
-                x = self.f[x][i]
-                y = self.f[y][i]
-
-        return self.f[x][0]
-
-    def dis(self, x: int, y: int) -> int:
-        return self.d[x] + self.d[y] - self.d[self.lca(x, y)] * 2
-
-
-MOD = 10**9 + 7
-N = 100010
-p2 = [0] * N
-
-
-def init():
-    p2[0] = 1
-    for i in range(1, N):
-        p2[i] = p2[i - 1] * 2 % MOD
-
-
-init()
-
+from math import log2
 
 class Solution:
-    def assignEdgeWeights(
-        self, edges: List[List[int]], queries: List[List[int]]
-    ) -> List[int]:
-        lca = LCA(edges, 1)
-        m = len(queries)
-        res = [0] * m
+    MOD = 10**9 + 7
 
-        for i in range(m):
-            x, y = queries[i][0], queries[i][1]
-            if x != y:
-                res[i] = p2[lca.dis(x, y) - 1]
+    def dfs(self, node, parent):
+        self.ancestor[node][0] = parent
 
-        return res
+        for nei in self.adj[node]:
+            if nei == parent:
+                continue
+
+            self.depth[nei] = self.depth[node] + 1
+            self.dfs(nei, node)
+
+    def buildAncestorTable(self):
+        for j in range(1, self.cols):
+            for node in range(self.n):
+                prev = self.ancestor[node][j - 1]
+
+                if prev != -1:
+                    self.ancestor[node][j] = self.ancestor[prev][j - 1]
+
+    def findLCA(self, u, v):
+        if self.depth[u] < self.depth[v]:
+            u, v = v, u
+
+        # Lift u to the same depth as v
+        k = self.depth[u] - self.depth[v]
+
+        for j in range(self.cols):
+            if k & (1 << j):
+                u = self.ancestor[u][j]
+
+        if u == v:
+            return u
+
+        # Lift both nodes together
+        for j in range(self.cols - 1, -1, -1):
+            if self.ancestor[u][j] == -1:
+                continue
+
+            if self.ancestor[u][j] != self.ancestor[v][j]:
+                u = self.ancestor[u][j]
+                v = self.ancestor[v][j]
+
+        return self.ancestor[u][0]
+
+    def assignEdgeWeights(self, edges, queries):
+        self.n = len(edges) + 1
+        self.cols = int(log2(self.n)) + 1
+
+        self.adj = [[] for _ in range(self.n)]
+
+        for u, v in edges:
+            u -= 1
+            v -= 1
+
+            self.adj[u].append(v)
+            self.adj[v].append(u)
+
+        self.depth = [0] * self.n
+        self.ancestor = [[-1] * self.cols for _ in range(self.n)]
+
+        self.dfs(0, -1)
+        self.buildAncestorTable()
+
+        # Precompute powers of 2
+        pow2 = [1] * (self.n + 1)
+
+        for i in range(1, self.n + 1):
+            pow2[i] = (2 * pow2[i - 1]) % self.MOD
+
+        result = []
+
+        for u, v in queries:
+            u -= 1
+            v -= 1
+
+            lca = self.findLCA(u, v)
+
+            d = self.depth[u] + self.depth[v] - 2 * self.depth[lca]
+
+            if d == 0:
+                result.append(0)
+            else:
+                result.append(pow2[d - 1])
+
+        return result
