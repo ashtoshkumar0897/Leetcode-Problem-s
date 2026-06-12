186629a05d0b7794395bee88819fa8c2034b0cda
Ashutosh Kumar <139233867+ashtoshkumar0897@users.noreply.github.com>
2026-06-12 21:36:38 +0530

    Time: 933 ms (83.02%), Space: 178.3 MB (45.28%) - LeetHub

--- 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.py
+++ 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.py
@@ -1,89 +1,68 @@
-from math import log2
-
+MX = 100001
+mod = 10**9+7
+fac = [1]*MX
+for i in range(1,MX):
+    fac[i] = fac[i-1]*(i)%mod
+inv = [1]*MX
+inv[-1] = pow(fac[-1],mod-2,mod)
+for i in range(MX-2,-1,-1):
+    inv[i] = inv[i+1]*(i+1)%mod
 class Solution:
-    MOD = 10**9 + 7
-
-    def dfs(self, node, parent):
-        self.ancestor[node][0] = parent
-
-        for nei in self.adj[node]:
-            if nei == parent:
-                continue
-
-            self.depth[nei] = self.depth[node] + 1
-            self.dfs(nei, node)
-
-    def buildAncestorTable(self):
-        for j in range(1, self.cols):
-            for node in range(self.n):
-                prev = self.ancestor[node][j - 1]
-
-                if prev != -1:
-                    self.ancestor[node][j] = self.ancestor[prev][j - 1]
-
-    def findLCA(self, u, v):
-        if self.depth[u] < self.depth[v]:
-            u, v = v, u
-
-        # Lift u to the same depth as v
-        k = self.depth[u] - self.depth[v]
-
-        for j in range(self.cols):
-            if k & (1 << j):
-                u = self.ancestor[u][j]
-
-        if u == v:
-            return u
-
-        # Lift both nodes together
-        for j in range(self.cols - 1, -1, -1):
-            if self.ancestor[u][j] == -1:
-                continue
-
-            if self.ancestor[u][j] != self.ancestor[v][j]:
-                u = self.ancestor[u][j]
-                v = self.ancestor[v][j]
-
-        return self.ancestor[u][0]
-
-    def assignEdgeWeights(self, edges, queries):
-        self.n = len(edges) + 1
-        self.cols = int(log2(self.n)) + 1
-
-        self.adj = [[] for _ in range(self.n)]
-
-        for u, v in edges:
-            u -= 1
-            v -= 1
-
-            self.adj[u].append(v)
-            self.adj[v].append(u)
-
-        self.depth = [0] * self.n
-        self.ancestor = [[-1] * self.cols for _ in range(self.n)]
-
-        self.dfs(0, -1)
-        self.buildAncestorTable()
-
-        # Precompute powers of 2
-        pow2 = [1] * (self.n + 1)
-
-        for i in range(1, self.n + 1):
-            pow2[i] = (2 * pow2[i - 1]) % self.MOD
-
-        result = []
-
-        for u, v in queries:
-            u -= 1
-            v -= 1
-
-            lca = self.findLCA(u, v)
-
-            d = self.depth[u] + self.depth[v] - 2 * self.depth[lca]
-
-            if d == 0:
-                result.append(0)
+    def assignEdgeWeights(self, edges: List[List[int]], queries: List[List[int]]) -> List[int]:
+        n = len(edges)+1
+        deep = [0]*(n+1)
+        g = [[] for _ in range(n+1)]
+        q = [[] for _ in range(n+1)]
+        ans = [0]*(len(queries))
+        vis = [False] * (n+1)
+        father = [i for i in range(n+1)]
+        for u,v in edges:
+            g[u].append(v)
+            g[v].append(u)
+        for idx,[u,v] in enumerate(queries):
+            q[u].append((v,idx))
+            q[v].append((u,idx))
+        
+        def find(x):
+            while father[x]!=x:
+                father[x] = father[father[x]]
+                x = father[x]
+            return father[x]
+        
+        def tarjan(u,parent):
+            vis[u] = True
+            for v in g[u]:
+                if v == parent:
+                    continue
+                tarjan(v,u)
+                father[v] = u
+            
+            for node,idx in q[u]:
+                if vis[node]:
+                    ans[idx] = find(node)
+        tarjan(1,0)
+        def f(u,fa,d):
+            nonlocal deep
+            deep[u] = d
+            for v in g[u]:
+                if v==fa:continue
+                f(v,u,d+1)
+        f(1,0,0)
+        res = [0]*len(queries)
+        def comb(n,a):
+            return fac[n]*inv[a]*inv[n-a]%mod
+        def f1(x):
+            ret = 0
+            for i in range(1,x+1,2):
+                ret = (ret + comb(x,i))%mod
+            return ret
+        for i,[a,b] in enumerate(queries):
+            lca = ans[i]
+            x = deep[a]+deep[b] - 2*deep[lca]
+            if x == 0:
+                res[i] = 0
             else:
-                result.append(pow2[d - 1])
-
-        return result
+                res[i] = pow(2, x-1, mod)
+        return res
+        
+        
