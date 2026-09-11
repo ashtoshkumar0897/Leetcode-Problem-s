0a82d9b61684b7a431a1e7d99c48cd75362b1f65
Ashutosh Kumar <139233867+ashtoshkumar0897@users.noreply.github.com>
2026-09-11 13:08:23 +0530

    Time: 14 ms (71.43%), Space: 19.3 MB (72.53%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
@@ -4,18 +4,17 @@ class Solution:
         vis = [False] * 1000
         ans = 0
 
-        for i in range(n):
+        for i in  range(n):
             if digits[i] == 0:
                 continue
             for j in range(n):
                 if j == i:
                     continue
                 for k in range(n):
-                    if k == i or k == j or digits[k] % 2 != 0:
+                    if k == i or k ==j or digits[k] % 2 != 0:
                         continue
                     x = digits[i] * 100 + digits[j] * 10 + digits[k]
                     if not vis[x]:
                         vis[x] = True
                         ans += 1
-
         return ans
